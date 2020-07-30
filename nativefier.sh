f7c616ae53fcbe0d6a3ac96762bf3237d4906f02
Peter Galantha <peter.galantha@yahoo.com>
2020-07-30 08:44:11 -0700

    app level customization

--- nativefier.sh
+++ nativefier.sh
@@ -22,46 +22,57 @@ else
 fi
 
 #GLOBAL_OPTIONS="--counter --bounce --show-menu-bar --fast-quit --single-instance --darwin-dark-mode-support --internal-urls .\*?"
-GLOBAL_OPTIONS="--counter --bounce --show-menu-bar --fast-quit --single-instance --darwin-dark-mode-support --internal-urls google.com|okta.com|nest.com"
+#GLOBAL_OPTIONS="--counter --bounce --show-menu-bar --fast-quit --single-instance --darwin-dark-mode-support --internal-urls google.com|okta.com|nest.com"
+GLOBAL_OPTIONS="--counter --bounce --show-menu-bar --fast-quit --single-instance --darwin-dark-mode-support"
 
 function create_app {
-    name=$1
-    url=$2
-    icon=$3
-    user_agent=$4
+    # sometimes we need to lie about user agent: https://github.com/jiahaog/nativefier/issues/831
+    user_agent="Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0"
+    internal_urls="google.com|okta.com"
+    local OPTIND o a
+    while getopts ":n:u:i:a:d:" opt; do
+        case $opt in
+            n) name=$OPTARG ;;
+            u) url=$OPTARG ;;
+            i) icon=$OPTARG ;;
+            a)
+                 # sometimes we need to lie about user agent: https://github.com/jiahaog/nativefier/issues/831
+                 user_agent=$OPTARG ;;
+            d) internal_urls=$OPTARG ;;
+            \?) echo "Invalid option: $OPTARG" 1>&2 ;;
+            :) echo "Invalid option: $OPTARG requires an argument" 1>&2 ;;
+        esac
+    done
+    shift $((OPTIND -1))
     global_options=${GLOBAL_OPTIONS}
     target_dir=apps
     mkdir -p $target_dir
-    if [ "$user_agent" ]; then
-        nativefier $global_options $options \
-            --user-agent "$user_agent" \
-            --name "$name" \
-            --icon "${ICONS_LOCATION}/files/$icon" \
-            "$url" \
-            "$target_dir"
-    else
-        nativefier $global_options $options \
-            --name "$name" \
-            --icon "${ICONS_LOCATION}/files/$icon" \
-            "$url" \
-            "$target_dir"
-    fi
+    NAT_CMD="nativefier $global_options $options"
+    [[ $name ]] || return 1
+    [[ $url ]] || return 1
+    [[ $icon ]] || return 1
+    NAT_CMD="$NAT_CMD --name \"$name\" --icon ${ICONS_LOCATION}/files/$icon"
+    [[ $user_agent ]] && NAT_CMD="$NAT_CMD --user-agent \"$user_agent\""
+    [[ $internal_urls ]] && NAT_CMD="$NAT_CMD --internal-urls \"$internal_urls\""
+    NAT_CMD="$NAT_CMD \"$url\" \"$target_dir\""
+
+    echo "$NAT_CMD"
+    eval "$NAT_CMD"
 }
 
-create_app "Pluralsight Flow" http://yo/flowdemo pluralsight.png
-create_app Evernote https://www.evernote.com/ evernote.png
-create_app "Google Mail" https://mail.google.com/ gmail.png
-create_app "Google Calendar" https://calendar.google.com/ google-calendar.png
-create_app "Google Meet" https://meet.google.com/ google-meet.png
-create_app "Google Drive" https://drive.google.com/ google-drive.png
-# sometimes we need to lie about user agent: https://github.com/jiahaog/nativefier/issues/831
-create_app "Google Maps" https://maps.google.com/ google-maps.png "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0"
-create_app Facebook https://facebook.com/ facebook.png
-create_app Messenger https://messenger.com/ messenger.png
-create_app Workday https://wd5.myworkday.com/oath workday.png
-create_app Nest https://home.nest.com/ nest3.png "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0"
-create_app KidsNote https://www.kidsnote.com/home/ kidsnote.png
-create_app "Tiller Money" https://docs.google.com/spreadsheets/d/1uEMgWbnQN9AaBRr8Fkc86FPTj0ZSoCaawBtVhRZQBVE tiller-money.png "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0"
+create_app -n "Pluralsight Flow" -u http://yo/flowdemo -i pluralsight.png -d "google.com|okta.com|corp-apps.aws.oath.cloud"
+create_app -n Evernote -u https://www.evernote.com/ -i evernote.png -d evernote.com
+create_app -n "Google Mail" -u https://mail.google.com/ -i gmail.png -d google.com
+create_app -n "Google Calendar" -u https://calendar.google.com/ -i google-calendar.png -d google.com
+create_app -n "Google Meet" -u https://meet.google.com/ -i google-meet.png -d google.com
+create_app -n "Google Drive" -u https://drive.google.com/ -i google-drive.png -d google.com
+create_app -n "Google Maps" -u https://maps.google.com/ -i google-maps.png -d google.com
+create_app -n Facebook -u https://facebook.com/ -i facebook.png -d facebook.com
+create_app -n Messenger -u https://messenger.com/ -i messenger.png -d messenger.com
+create_app -n Workday -u https://wd5.myworkday.com/oath -i workday.png -d "google.com|okta.com|myworkday.com"
+create_app -n Nest -u https://home.nest.com/ -i nest3.png -d "google.com|nest.com"
+create_app -n KidsNote -u https://www.kidsnote.com/home/ -i kidsnote.png -d kidsnote.com
+create_app -n "Tiller Money" -u https://docs.google.com/spreadsheets/d/1uEMgWbnQN9AaBRr8Fkc86FPTj0ZSoCaawBtVhRZQBVE -i tiller-money.png -d google.com
 
 # DRM-protected content won't play: https://github.com/jiahaog/nativefier/issues/435
 # create_app Netflix https://www.netflix.com/ netflix.png
